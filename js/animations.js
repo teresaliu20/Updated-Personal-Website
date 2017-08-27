1d24f7278f233da0892039e0a0240567cc4d9982
teresaliu20 <teresali@usc.edu>
2017-08-27 16:31:27 -0700

    Fixed??

---
+++ js/animations.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    console.log( "Ready!" );
+    AOS.init();
+
+    $('#splash-section').hide().fadeIn(700);
+
+    $('#splash-section').css({
+		"display": "flex",
+		"justify-content": "center",
+		"align-items": "center"
+	});
+
+    var options = {
+	 	strings: ["", "ENGINEER. ARTIST. DEVELOPER."],
+	 	typeSpeed: 50,
+	}
+	$('#email-button').on('click', function() {
+		// window.open('teresali@usc.edu');
+		var link = "mailto:teresali@usc.edu";
+    	 window.location.href = link;
+	})
+	var typed = new Typed("#catch-phrase", options);
+});
